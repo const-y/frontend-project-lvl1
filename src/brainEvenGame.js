606f7ed381dfd9ae064f0c59c016696b48a5dedd
Konstantin Yarish <pm-mc@mail.ru>
2021-01-30 22:16:31 +0300

    Add link to asciinema to readme

--- src/brainEvenGame.js
+++ src/brainEvenGame.js
@@ -1,16 +1,16 @@
 import { question } from 'readline-sync';
-import _ from 'lodash';
 
-const LOWER = 1;
 const UPPER = 100;
 const STAGES = 3;
 const YES = 'yes';
 const NO = 'no';
 
+const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+
 const isEven = (value) => value % 2 === 0;
 
 const generateQuestion = () => {
-  const value = _.random(LOWER, UPPER);
+  const value = getRandomInt(UPPER);
   const correctAnswer = isEven(value) ? YES : NO;
   return { value, correctAnswer };
 };
