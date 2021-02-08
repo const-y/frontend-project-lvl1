ec60f65a2efb4fffa7b9a630ccdc7cebd69b83a9
Konstantin Yarish <pm-mc@mail.ru>
2021-02-08 23:33:56 +0300

    Add brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import game from '../src/index.js';
+import brainProgression from '../src/games/brainProgression.js';
+
+game(brainProgression);
---
+++ src/games/brainProgression.js
@@ -0,0 +1,26 @@
+import { getRandomInt } from '../utils.js';
+
+const arithmeticalProgression = (length, d) => {
+  const result = [];
+  let current = getRandomInt();
+  while (result.length < length) {
+    result.push(current);
+    current += d;
+  }
+  return result;
+};
+
+const generateQuestion = () => {
+  const length = 10;
+  const d = getRandomInt();
+  const hiddenIndex = getRandomInt(length - 1);
+  const regression = arithmeticalProgression(length, d);
+  const value = regression.map((item, index) => (index === hiddenIndex ? '..' : item)).join(' ');
+  const correctAnswer = String(regression[hiddenIndex]);
+  return { value, correctAnswer };
+};
+
+export default {
+  description: 'What number is missing in the progression?',
+  generateQuestion,
+};
