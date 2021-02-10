e45fe0192f8f834247951a2a67f1331a6779788c
Konstantin Yarish <pm-mc@mail.ru>
2021-02-11 01:11:59 +0300

    Refactor game()

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import sayHello from './cli.js';
 
 const stagesCount = 3;
 
-const game = ({ description, generateQuestion }) => {
-  const stage = (counter) => {
-    if (counter === 0) {
-      return true;
-    }
-    const { value, correctAnswer } = generateQuestion();
-    console.log(`Question: ${value}`);
-    const answer = question('Your answer: ');
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-      const newCounter = counter - 1;
-      return stage(newCounter);
-    }
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    return false;
-  };
+const stage = (generateQuestion, counter) => {
+  if (counter === 0) {
+    return true;
+  }
+  const { value, correctAnswer } = generateQuestion();
+  console.log(`Question: ${value}`);
+  const answer = question('Your answer: ');
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    const newCounter = counter - 1;
+    return stage(newCounter);
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  return false;
+};
 
+const game = ({ description, generateQuestion }) => {
   console.log('Welcome to the Brain Games!');
   const name = sayHello();
   console.log(description);
-  const win = stage(stagesCount);
+  const win = stage(generateQuestion, stagesCount);
   if (win) {
     console.log(`Congratulations, ${name}!`);
   } else {
