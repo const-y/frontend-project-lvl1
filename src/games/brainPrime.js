a8fcf8a14dabfb4d0f966fb22ee0e6db84e47c27
Konstantin Yarish <pm-mc@mail.ru>
2021-02-14 18:53:03 +0300

    Use guard expression in isPrime()

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -2,10 +2,13 @@ import getRandomInt from '../getRandomInt.js';
 import { yes, no } from '../index.js';
 
 const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
   for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
     if (num % i === 0) return false;
   }
-  return num > 1;
+  return true;
 };
 
 const generateQuestionAnswerPair = () => {
