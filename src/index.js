7620124da55af283042a20130e2d89023c453d6c
Konstantin Yarish <pm-mc@mail.ru>
2021-02-19 23:01:07 +0300

    Remove function stage

--- src/index.js
+++ src/index.js
@@ -2,28 +2,24 @@ import { question } from 'readline-sync';
 
 const stagesCount = 3;
 
-const stage = (generateQuestionAnswer, counter) => {
-  if (counter === 0) {
-    return true;
-  }
-  const { question: gameQuestion, correctAnswer } = generateQuestionAnswer();
-  console.log(`Question: ${gameQuestion}`);
-  const answer = question('Your answer: ');
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-    const newCounter = counter - 1;
-    return stage(generateQuestionAnswer, newCounter);
-  }
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  return false;
-};
-
 const play = ({ description, generateQuestionAnswer }) => {
   console.log('Welcome to the Brain Games!');
   const name = question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(description);
-  const win = stage(generateQuestionAnswer, stagesCount);
+  let win = true;
+  for (let i = 1; i <= stagesCount; i += 1) {
+    const { question: gameQuestion, correctAnswer } = generateQuestionAnswer();
+    console.log(`Question: ${gameQuestion}`);
+    const answer = question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      win = false;
+      break;
+    }
+  }
   if (win) {
     console.log(`Congratulations, ${name}!`);
   } else {
